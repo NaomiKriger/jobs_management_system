71901832b34b5c81b735525e08c98b732cca3dc5
NaomiKriger <naomikriger@gmail.com>
2023-04-01 12:56:06 +0300

    test_client fixture, and pytest instead of unittest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,74 +1,79 @@
 from http.client import BAD_REQUEST, OK
-from unittest import TestCase
+
+import pytest
 
 from app import app
 
 
-class TestIndex(TestCase):
-    def test_index(self):
-        with app.test_client() as test_client:
-            response = test_client.get("/")
-            assert response.status_code == OK
-            json_response = response.get_json()
-            assert json_response == {
-                "message": "Welcome to the jobs management system!"
-            }
-            assert "Welcome to the jobs management system!" in response.text
+@pytest.fixture
+def test_client():
+    with app.test_client() as test_client:
+        yield test_client
 
 
-class TestConfigureNewEvent(TestCase):
-    def test_valid_input(self):
-        data = {
-            "event_name": "my_event",
-            "schema": {
-                "type": "object",
-                "properties": {"name": {"type": "string"}, "age": {"type": "number"}},
-                "required": ["name", "age"],
-            },
-        }
-        with app.test_client() as test_client:
-            response = test_client.post("/configure_new_event", json=data)
-            assert response.status_code == OK
-            assert response.text == f"event {data.get('event_name')} added to the DB"
+# TEST INDEX #
 
-    def test_missing_parameter(self):
-        # missing event_name parameter
-        data = {
+def test_index(test_client):
+    response = test_client.get("/")
+    assert response.status_code == OK
+    json_response = response.get_json()
+    assert json_response == {
+        "message": "Welcome to the jobs management system!"
+    }
+    assert "Welcome to the jobs management system!" in response.text
+
+
+# TEST CONFIGURE NEW EVENT #
+
+def test_valid_input(test_client):
+    data = {
+        "event_name": "my_event",
+        "schema": {
             "type": "object",
             "properties": {"name": {"type": "string"}, "age": {"type": "number"}},
             "required": ["name", "age"],
-        }
-        with app.test_client() as test_client:
-            response = test_client.post("/configure_new_event", json=data)
-            assert response.status_code == BAD_REQUEST
-            assert response.text == f"missing required parameter: event_name"
+        },
+    }
+    response = test_client.post("/configure_new_event", json=data)
+    assert response.status_code == OK
+    assert response.text == f"event {data.get('event_name')} added to the DB"
 
-        # missing schema parameter
-        with app.test_client() as test_client:
-            response = test_client.post(
-                "/configure_new_event", json={"event_name": "my_event"}
-            )
-            assert response.status_code == BAD_REQUEST
-            assert response.text == f"missing required parameter: schema"
 
-    def test_invalid_parameter_type(self):
-        # event_name is not a string
-        data = {
-            "event_name": 123,
-            "schema": {
-                "type": "object",
-                "properties": {"name": {"type": "string"}, "age": {"type": "number"}},
-                "required": ["name", "age"],
-            },
-        }
-        with app.test_client() as test_client:
-            response = test_client.post("/configure_new_event", json=data)
-            assert response.status_code == BAD_REQUEST
-            assert response.text == "event_name should be a string"
+def test_missing_parameter(test_client):
+    # missing event_name parameter
+    data = {
+        "type": "object",
+        "properties": {"name": {"type": "string"}, "age": {"type": "number"}},
+        "required": ["name", "age"],
+    }
+    response = test_client.post("/configure_new_event", json=data)
+    assert response.status_code == BAD_REQUEST
+    assert response.text == f"missing required parameter: event_name"
 
-        # schema is not a json
-        data = {"event_name": "my_event", "schema": "hey there"}
-        with app.test_client() as test_client:
-            response = test_client.post("/configure_new_event", json=data)
-            assert response.status_code == BAD_REQUEST
-            assert response.text == "schema should be a json"
+    # missing schema parameter
+    response = test_client.post(
+        "/configure_new_event", json={"event_name": "my_event"}
+    )
+    assert response.status_code == BAD_REQUEST
+    assert response.text == f"missing required parameter: schema"
+
+
+def test_invalid_parameter_type(test_client):
+    # event_name is not a string
+    data = {
+        "event_name": 123,
+        "schema": {
+            "type": "object",
+            "properties": {"name": {"type": "string"}, "age": {"type": "number"}},
+            "required": ["name", "age"],
+        },
+    }
+    response = test_client.post("/configure_new_event", json=data)
+    assert response.status_code == BAD_REQUEST
+    assert response.text == "event_name should be a string"
+
+    # schema is not a json
+    data = {"event_name": "my_event", "schema": "hey there"}
+    response = test_client.post("/configure_new_event", json=data)
+    assert response.status_code == BAD_REQUEST
+    assert response.text == "schema should be a json"
