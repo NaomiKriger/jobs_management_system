e5a00629effd3bfc5b7152e39acde2b87aebe050
NaomiKriger <naomikriger@gmail.com>
2023-04-11 07:59:35 +0300

    modified sample_job_1

--- tests/sample_jobs/sample_job_1/job_1.py
+++ tests/sample_jobs/sample_job_1/job_1.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -9,29 +8,30 @@ app = Flask(__name__)
 versions_and_sources = {}
 
 
-@app.route("/scrape_versions_and_sources_from_table", methods=['GET'])
+@app.route("/scrape_versions_and_sources_from_table", methods=["POST"])
 def scrape_versions_and_sources_from_table() -> dict:
-    source_name = os.getenv("source_name")
-    url = os.getenv("url")
+    data = request.values.to_dict() if request.values else request.get_json()
+    source_name = data.get("source_name")
+    url = data.get("url")
     if not source_name or not url or not isinstance(source_name, str) or not isinstance(url, str):
         return {}
 
     source = requests.get(url).text
-    soup = BeautifulSoup(source, 'html.parser')
-    tables = soup.find_all('table', class_='styled-table')
+    soup = BeautifulSoup(source, "html.parser")
+    tables = soup.find_all("table", class_="styled-table")
 
     dict_to_update = {source_name: {}}
 
     for table in tables:
-        current_table = pd.read_html(str(table))[0][['SOURCE', 'VERSION']]
+        current_table = pd.read_html(str(table))[0][["SOURCE", "VERSION"]]
         for index, row in current_table.iterrows():
             if row.VERSION not in dict_to_update[source_name]:
-                dict_to_update[source_name].update({row.VERSION: {'source': row.SOURCE, 'tested': False}})
+                dict_to_update[source_name].update({row.VERSION: {"source": row.SOURCE, "tested": False}})
             if len(dict_to_update[source_name]) >= 10:
                 break
     versions_and_sources.update(dict_to_update)
     return versions_and_sources
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
