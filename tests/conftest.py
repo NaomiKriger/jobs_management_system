87dceafaa23b1751429d5d38d2e777505b607704
NaomiKriger <naomikriger@gmail.com>
2023-04-16 11:17:54 +0300

    variable renamed

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from tests.mocks import basic_schema_mock
 
 load_dotenv()
 
-event_pre_configured_in_db = "event_pre_configured_in_db"
+event_name_pre_configured_in_db = "event_name_pre_configured_in_db"
 
 
 @pytest.fixture(scope="session")
@@ -21,7 +21,7 @@ def test_client():
     with app.app_context():
         db.init_app(app)
         db.create_all()
-        db.session.add(Event(event_pre_configured_in_db, basic_schema_mock), db)
+        db.session.add(Event(event_name_pre_configured_in_db, basic_schema_mock), db)
         db.session.commit()
         yield app.test_client()
         db.session.rollback()
